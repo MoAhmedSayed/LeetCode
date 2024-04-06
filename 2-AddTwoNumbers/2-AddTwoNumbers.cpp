42d7ab22adbb335156d2f7edbb2eba0f068f5e03
Mohamed Ahmed Sayed <43236699+MoAhmedSayed@users.noreply.github.com>
2024-04-06 03:36:21 +0100

    Update file 06/04/2024, 03:36:21

--- 2-AddTwoNumbers/2-AddTwoNumbers.cpp
+++ 2-AddTwoNumbers/2-AddTwoNumbers.cpp
@@ -1,24 +1,24 @@
-                incrementalValue = currentValue;
-            }
-            ListNode temp = *(new ListNode(incrementalValue));
-            current->next = new ListNode(0);
-            *(current->next) = temp;
-            if (currentL1 != nullptr)
-            {
-                currentL1 = currentL1->next;
-            }
-            if (currentL2 != nullptr)
-            {
-                currentL2 = currentL2->next;
-            }
-            current = current->next;
-        }
-        if (carry > 0)
-        {
-            current->next = new ListNode(carry);
-        }
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        int carry = 0;
 
-        return head->next;
-    }
+        ListNode* head = new ListNode(0, nullptr);
+        ListNode* current = head;
 
+        ListNode* currentL1 = l1;
+        ListNode* currentL2 = l2;
+        int incrementalValue = 0;
+
+        while (currentL1 != nullptr || currentL2 != nullptr) {
 [
